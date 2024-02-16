ae7c919a3d4cea60bc20bdae2b1151b12bd93f48
Tony Jenkins <tony@tony-jenkins.co.uk>
2024-02-16 14:45:33 +0000

    Refactor to use 404 shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -15,12 +15,10 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        context = {
-            'question': Question.objects.get(pk=question_id),
-        }
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
+
+    context = {
+        'question': get_object_or_404(Question, pk=question_id),
+    }
 
     return render(request, 'polls/detail.html', context)
 
