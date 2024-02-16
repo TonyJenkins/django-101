40731736aef6857c113e69196d8a6c7693f2a2bd
Tony Jenkins <tony@tony-jenkins.co.uk>
2024-02-16 14:42:36 +0000

    Display question from link on index, or 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -16,7 +15,14 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        context = {
+            'question': Question.objects.get(pk=question_id),
+        }
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
